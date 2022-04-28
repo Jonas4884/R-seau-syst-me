46b13eb2feeb74f462031c7f3f14d853fe1f8590
Jonas4884 <hei.jonatana@gmail.com>
2022-04-28 12:13:16 +0300

    Début de l'algorithme//Comptez chaque couleurs

---
+++ base_javascript/yaourt.js
@@ -0,0 +1,11 @@
+//ceci est une approche au solution du battledev niveau 2 sur Isograd
+let diff = ['rouge','jaune','blanc','rouge','jaune','noir','jaune','rouge','rouge'];
+let color = [diff[0]];
+let stock = [0];
+//Rah mbola ts ao le color de push-ena anatnle tableau vide
+for (let i = 1; i < diff.length; i++) {
+    if (diff[0] != diff[i] && color.indexOf(diff[i]) == -1) {
+        color.push(diff[i]);
+        stock.push(0);
+    }
+}
