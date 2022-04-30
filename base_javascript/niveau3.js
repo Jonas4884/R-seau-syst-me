b455e5ccf3a4729f4406cefd7d8bbbee404240a8
Jonas4884 <hei.jonatana@gmail.com>
2022-04-30 08:04:19 +0300

    fix battle dev niveau 3 feat test unitaire

---
+++ base_javascript/niveau3.js
@@ -0,0 +1,11 @@
+//Ceci est une approche aux problemes du battle dev de SuperDigit(un sujet de difficulté niveau 3)
+export function superDigit(p){
+    /*Ceci est une solution aux test unitaire*/
+	if(p === undefined){
+		throw new Error("Invalid value");
+	}else{
+		if(p.length === 0){
+			throw new Error("Invalid value");
+		}
+	}
+    /*Début de l'algorithme*/
